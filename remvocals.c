b703a13b0bbd7d0c57f295260218167ec205018f
rudyarb <106043414+rudyarb@users.noreply.github.com>
2024-01-30 22:35:40 -0500

    Finished remvocals.c

--- remvocals.c
+++ remvocals.c
@@ -24,7 +24,29 @@ int main(int argc, char **argv) {
     
     // Write to dest_file
     fwrite(&to_write, 1, 44, source_file);
-    
+
+
+    // Dealing with the rest of the bytes as shorts
+    short size[2];
+
+    // while loop to iterate in steps of 2
+    while (fread(size, 1, sizeof(size), source_file) > 0) {
+        
+        short left = size[0];
+        short right = size[1];
+
+        // Computation and assigning of pointer:
+
+        short *combined_ptr = malloc(sizeof(short));
+        short combined = (left - right) / 2;
+        combined_ptr = &combined;
+
+        // Write to file
+
+        fwrite(combined_ptr, sizeof(short), 1, dest_file);
+
+    }
+
     fclose(source_file);
     fclose(dest_file);
 }
